fc95a413570269f76c1e131d99a58aa74ddd9dd0
Sébastien BRUNO <sebastien.bruno@gmail.com>
2021-10-25 22:15:52 +0200

    Integration of MongoDB database with the mongoose connector

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://sebastienbruno:HvoNvRA2B40PX04e@cluster0.rw1bt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
